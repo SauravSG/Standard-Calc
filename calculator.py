cf709dcfa6f200a13a4c52f6069898c26361a301
SauravSG <sauravganc@gmail.com>
2021-11-22 15:16:22 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -6,7 +6,7 @@ st.title("Standard Calculator")
 first_number = st.text_input("Enter first number", "0")
 second_number = st.text_input("Enter second number", "0")
 
-operation = st.selectbox("Select operation:",["Addition", "Substraction", "Multiplication", "Division"])
+operation = st.selectbox("Select operation:",["Addition", "Substraction", "Multiplication", "Division", "Modulus Division"])
 
 if st.button("Perform Operation"):
 	if operation=="Addition":
@@ -21,8 +21,8 @@ if st.button("Perform Operation"):
 	elif operation =="Division":
 		result = int(first_number) / int(second_number)
 		st.success(f"Your result is {result}")
-	# elif operation =="Modular Devision":
-	# 	result = int(first_number) % int(second_number)
-	# 	st.success(f"Your result is {result}")
+	elif operation == "Modulus Division":
+		result = int(first_number) % int(second_number)
+		st.success(f"Your result is {result}")
 
 
